9b0160365d98529b3edf5d05dfb4a16f15f8c103
Fredy <fredrodrigues795@gmail.com>
2024-04-07 02:20:27 +0100

    Area da imagem terminada

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,143 @@
-// import { Icon } from '@iconify/react';
-import Categorias from "./components/Categorias";
+import {useState} from 'react';
+import {Icon} from '@iconify/react';
 import Brand from "./components/Brand";
-import { BrowserRouter as Router, Route, Routes, Navigate, Link } from "react-router-dom";
+import Categorias from "./components/Categorias";
+import {BrowserRouter as Router, Route, Routes, Navigate, Link, useLocation} from "react-router-dom";
+import {Text, Box, Flex, Image, Link as ChakraLink} from "@chakra-ui/react";
 
 export default function App() {
   return (
     <Router>
-      <main>
-        <ul>
-          <li><Link to="/categorias">Categorias</Link></li>
-          <li><Link to="/">Brand</Link></li>
-        </ul>
-        <Routes>
-          <Route path="/" element={ <Brand /> }/>
-          <Route path="/categorias" exact element={ <Categorias /> }/>
-          <Route path="*" element={ <Navigate to="/" replace/> }/>
-        </Routes>
-      </main>
+      <AppContent />
     </Router>
+  );
+}
+
+function AppContent() {
+  const [barraLateral, setBarraLateral] = useState(false)
+  const rota = useLocation().pathname
+
+  return (
+    <main>
+      {/* Cabeçalho para Computador */}
+      {rota !== "/categorias" ? (
+        <>
+          <Box display={{ base: "none", xl: "block" }}>
+            <Box as="header" display="flex" justifyContent="center" padding="16px" margin="auto">
+              <Flex width="100%" justifyContent="space-between" maxWidth="1268px" alignItems="center">
+                <Flex alignItems="center" paddingRight="16px">
+                  <ChakraLink as={Link} to="/" maxWidth="120px" maxHeight="40px">
+                    <Image src="https://assets.99static.com/logos/en/wordmark_grey.svg" alt="Logo" />
+                  </ChakraLink>
+                  <Flex color="black" marginLeft="32px" fontWeight="600">
+                    <Text marginLeft="24px">
+                      <ChakraLink as={Link} to="/categorias" _hover={{textDecoration: "none"}}>Categorias</ChakraLink>
+                    </Text>
+                    <Text marginLeft="24px">
+                      <ChakraLink as={Link} to="/" _hover={{textDecoration: "none"}}>Como funciona</ChakraLink>
+                    </Text>
+                    <Text marginLeft="24px">
+                      <ChakraLink as={Link} to="/" _hover={{textDecoration: "none"}}>Encontre um designer</ChakraLink>
+                    </Text>
+                    <Text marginLeft="24px">
+                      <ChakraLink as={Link} to="/" _hover={{textDecoration: "none"}}>Inspiração</ChakraLink>
+                    </Text>
+                    <Text marginLeft="24px">
+                      <ChakraLink as={Link} to="/" _hover={{textDecoration: "none"}}>Pro</ChakraLink>
+                    </Text>
+                  </Flex>
+                </Flex>
+                <Flex color="black" fontWeight="600">
+                  <Flex alignItems="center">
+                    <Icon width="25px" icon="heroicons:phone-16-solid" />
+                    <Text marginLeft="24px">
+                      <ChakraLink as={Link} to="/">+1 877 834 4534</ChakraLink>
+                    </Text>
+                  </Flex>
+                  <Flex alignItems="center" marginLeft="24px">
+                    <Text marginLeft="24px">
+                      <ChakraLink as={Link} to="/">Fazer login</ChakraLink>
+                    </Text>
+                  </Flex>
+                </Flex>
+              </Flex>
+            </Box>
+          </Box>
+        </>
+      ) : (
+        null
+      )}
+      
+      {/* Cabeçalho para Celular */}
+      <Box display={{ base: "block", xl: "none" }}>
+        {/* Cabeçalho */}
+        <Box as="header" padding="16px" margin="auto" display="flex" alignItems="center">
+          <Flex width="100%" justifyContent="space-between" alignItems="center" padding="0px 16px">
+            <Box flex="1" onClick={() => setBarraLateral(!barraLateral)}>
+              <Icon width="25px" height="40px" icon="heroicons:bars-3-16-solid" />
+            </Box>
+            <Flex flex="1" justifyContent="center">
+              <ChakraLink as={Link} to="/" maxWidth="120px" maxHeight="40px">
+                <Image draggable="false" src="https://assets.99static.com/logos/en/wordmark_grey.svg" alt="Logo" />
+              </ChakraLink>
+            </Flex>
+            <Box flex="1">
+              <Text textAlign="end" color="black" fontWeight="600" marginLeft="24px">
+                <ChakraLink as={Link} to="/" _hover={{textDecoration: "none"}}>Fazer login</ChakraLink>
+              </Text>
+            </Box>
+          </Flex>
+        </Box>
+
+        {/* Barra lateral */}
+        <Box zIndex="200" position="absolute" top="0" left="0" display={barraLateral ? "flex" : "none"} width="100%">
+          <Box backgroundColor="white" minWidth="320px" maxWidth="30vw" height="100vh" padding="12px 16px">
+            <Box display="flex" flexDirection="column">
+              <Flex alignItems="center" padding="12px 0px">
+                <Icon width="25px" icon="heroicons:bars-3-16-solid" onClick={() => setBarraLateral(false)} />
+                <Text marginLeft="24px">
+                </Text>
+              </Flex>
+              <Flex alignItems="center" padding="12px 0px">
+                <Icon width="25px" icon="heroicons:home-20-solid" />
+                <Text marginLeft="24px">
+                  <ChakraLink as={Link} to="/" _hover={{textDecoration: "none"}} onClick={() => setBarraLateral(false)}>Página inicial</ChakraLink>
+                </Text>
+              </Flex>
+              <Flex alignItems="center" padding="12px 0px">
+                <Icon width="25px" icon="heroicons:home-20-solid" />
+                <Text marginLeft="24px">
+                  <ChakraLink as={Link} to="/categorias" _hover={{textDecoration: "none"}} onClick={() => setBarraLateral(false)}>Categorias</ChakraLink>
+                </Text>
+              </Flex>
+              <Flex alignItems="center" padding="12px 0px">
+                <Icon width="25px" icon="heroicons:home-20-solid" />
+                <Text marginLeft="24px">
+                  <ChakraLink as={Link} to="/" _hover={{textDecoration: "none"}} onClick={() => setBarraLateral(false)}>Encontre um designer</ChakraLink>
+                </Text>
+              </Flex>
+              <Flex alignItems="center" padding="12px 0px">
+                <Icon width="25px" icon="heroicons:home-20-solid" />
+                <Text marginLeft="24px">
+                  <ChakraLink as={Link} to="/" _hover={{textDecoration: "none"}} onClick={() => setBarraLateral(false)}>Inspiração</ChakraLink>
+                </Text>
+              </Flex>
+              <Flex alignItems="center" padding="12px 0px">
+                <Icon width="25px" icon="heroicons:home-20-solid" />
+                <Text marginLeft="24px">
+                  <ChakraLink as={Link} to="/" _hover={{textDecoration: "none"}} onClick={() => setBarraLateral(false)}>Pro</ChakraLink>
+                </Text>
+              </Flex>
+            </Box>
+          </Box>
+          <Box onClick={() => setBarraLateral(false)} flex="1" height="100vh" backgroundColor="rgba(0, 0, 0, 0.5)"></Box>
+        </Box>
+      </Box>
+      <Routes>
+        <Route path="/" element={<Brand />} />
+        <Route path="/categorias" exact element={<Categorias />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </main>
   )
 }
--- src/components/Brand.jsx
+++ src/components/Brand.jsx
@@ -1,9 +1,67 @@
-// import { Icon } from '@iconify/react';
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import { Text, Box, Flex, Image, Link as ChakraLink } from "@chakra-ui/react";
 
 export default function Brand() {
+  const [fundoAtual, setFundoAtual] = useState(null);
+  const fundos = [
+    {
+      id: 1,
+      url: "https://images.ctfassets.net/n5k68ty3nctt/4q8QgGmJ0tNMqJVK7nO4Cd/fa288a5e3fee8724371f4267015ff263/brand-identity-pack.avif?fm=avif&q=80"
+    },
+    {
+      id: 2,
+      url: "https://images.ctfassets.net/n5k68ty3nctt/7dzp2l7SwmUOoV4OmcgHAn/5c842fa7aa4296e43becd926f3660659/logo-and-brand-identity-kit.avif?fm=avif&q=80"
+    }
+  ];
+
+  useEffect(() => {
+    setFundoAtual(fundos[0]);
+  }, []);
+
   return (
-    <>
-    Brand
-    </>
-  )
+    <Flex paddingTop="48px" justifyContent="center">
+      <Box width="100%" maxWidth="1268px" padding={{ base: "0px 32px", lg: "0px" }} margin={{ base: "0px", lg: "0px 32px" }}>
+        <Flex gap="64px" justifyContent="space-between">
+          <Box width="100%" maxWidth="716px">
+            <Flex flexDirection="column">
+              <Box maxHeight="525px">
+                <Image height="100%" maxHeight="100%" draggable="false" src={fundoAtual ? fundoAtual.url : ""} alt="Fundo" />
+              </Box>
+              <Flex marginTop="20px" justifyContent="space-between">
+                <Flex>
+                  {fundos.map((fundo, index) => (
+                    <Box key={fundo.id} marginRight={index === 0 ? "16px" : "0"} width="15px" height="15px" cursor="pointer" borderRadius="100%" backgroundColor={fundoAtual && fundoAtual.id === fundo.id ? "#333" : "#ddd"}
+                      onClick={() => setFundoAtual(fundo)}
+                    ></Box>
+                  ))}
+                </Flex>
+                <Box>
+                  <Text color="rgb(91, 90, 88)">Concurso de design criado por
+                  <ChakraLink as={Link} to="/" color="rgb(91, 90, 88)" fontWeight="500" textDecoration="underline 2px solid"> The Hype Lab</ChakraLink>
+                  </Text>
+                  <Text textAlign="end" color="rgb(91, 90, 88)">Design vencedor por <ChakraLink as={Link} to="/" color="rgb(91, 90, 88)" fontWeight="500" textDecoration="underline 2px solid">leonelbalza</ChakraLink></Text>
+                </Box>
+              </Flex>
+            </Flex>
+            <Flex fontSize="20px" fontWeight="700">
+              <Box>
+                <Text>O que está incluído</Text>
+              </Box>
+              <Box>
+                <Text>Como funciona</Text>
+              </Box>
+              <Box>
+                <Text>Preços</Text>
+              </Box>
+              <Box>
+                <Text>Perguntas Frequentes</Text>
+              </Box>
+            </Flex>
+          </Box>
+          <Box display={{ base: "none", lg: "block" }} flex="1" minWidth="270px" height="100vh" backgroundColor="yellow"></Box>
+        </Flex>
+      </Box>
+    </Flex>
+  );
 }
